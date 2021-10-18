75229d0c13482de66d00c4d00fb32b78d68a4ff3
Vishinky-Oleg <vishinsky.oleg@gmail.com>
2021-10-18 23:10:12 +0300

    Commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,34 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/app.ts",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        publicPath: "dist",
-    },
-    devtool: "inline-source-map",
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    devServer: {
-        static: "./",
-    },
+	// mode: "development",
+	entry: "./src/app.ts",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "dist",
+	},
+	// devtool: "inline-source-map",
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "TS Drag&Drop",
+			template: "./index.html",
+		}),
+	],
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	devServer: {
+		static: "./",
+	},
 };
